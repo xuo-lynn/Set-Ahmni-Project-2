0ba69e99f82e725dabe3e408c41434ddf8a1b34b
xuo <61861557+xuo-lynn@users.noreply.github.com>
2023-03-10 19:54:10 -0500

    author  update

---
+++ bookstore/src/main/java/com/company/bookstore/Repository/AuthorRepo.java
@@ -0,0 +1,13 @@
+package com.company.bookstore.Repository;
+
+import com.company.bookstore.Model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuthorRepo extends JpaRepository<Author, Integer> {
+    List<Author> findByAuthorId(Integer authorId);
+
+}
